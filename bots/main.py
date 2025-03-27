4a45539228aacbb777a05306cf77aa8d6fdb9526
yahaha5788 <155703491+yahaha5788@users.noreply.github.com>
2025-03-27 12:41:35 -0400

    oh boy this is going to take a lot of commits

--- bots/main.py
+++ bots/main.py
@@ -1,2 +1,8 @@
 #this will be the file to run all the bots
 #might also have some logging stuff too
+
+from polaroidsupercolorinstantcamera import bot as polaroid
+from harold import bot as harold
+from robobot import bot as robobot
+
+import python-dotenv
