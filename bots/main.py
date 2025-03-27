624dfe97c0efb72ad0f60e90a388c9ebfef4826c
yahaha5788 <155703491+yahaha5788@users.noreply.github.com>
2025-03-27 12:58:58 -0400

    should be good

--- bots/main.py
+++ bots/main.py
@@ -5,4 +5,10 @@ from polaroidsupercolorinstantcamera import bot as polaroid
 from harold import bot as harold
 from robobot import bot as robobot
 
-import python-dotenv
+from dotenv import dotenv_values
+
+config = dotenv_values("C:/Users/thomp/bots/keys.env")
+
+polaroid.run(config["POLAROID_KEY"]
+robobot.run(config["ROBOBOT_KEY"]
+harold.run(config["HAROLD_KEY"]
